517301cf93918cfeb8f042fc8b017c3c0d16e06d
flavio10araujo <flavio10araujo@yahoo.com.br>
2023-03-29 10:46:51 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Buddy TM!</title>
+        <meta name="description" content="Buddy TM!" />
+      </Head>
+    </Fragment>
+  );
+}
+
+export default HomePage;
